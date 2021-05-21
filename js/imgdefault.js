57dfef19c1a7f11ee0e63efd721716af1d17a909
vangona <rlarhksrud12@naver.com>
2021-05-22 00:55:26 +0900

    2021.05.22
    - default img update

---
+++ js/imgdefault.js
@@ -0,0 +1,6 @@
+const firstImg = document.getElementById("kwankyoung");
+const scondImg = document.getElementById("kyoungmin");
+
+const imgDefault = function imgDefault(event){
+    console(event.taget)
+}
